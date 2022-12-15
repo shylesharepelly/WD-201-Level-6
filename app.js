67bf90f3e2630652c6c98a13cf886ba317943818
Shylesh <shylesharepelly@gmail.com>
2022-12-15 22:28:54 +0530

    init

--- app.js
+++ app.js
@@ -3,29 +3,28 @@ const app = express();
 const { Todo } = require("./models");
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
+app.set("view engine","ejs");
+const path = require('path');
 
-app.get("/", function (request, response) {
-  response.send("Hello World");
+
+app.use(express.static(path.join(__dirname,"public")));
+
+app.get("/",async  function (request, response) {
+  const allTodos = await Todo.getTodos();
+  if(request.accepts('html')){
+    response.render('index',{
+      allTodos
+    });
+  }
+  else{
+    response.json({allTodos})
+  }
+  
 });
 
-app.get("/todos", async function (_request, response) {
-  console.log("Processing list of all Todos ...");
-  // FILL IN YOUR CODE HERE
-
-  // First, we have to query our PostgerSQL database using Sequelize to get list of all Todos.
-  // Then, we have to respond with all Todos, like:
-  // response.send(todos)
-try{
-const todo = await Todo.findAll({order: [["id","ASC"]]});
-return response.json(todo)
-}
-catch(error){
-console.log(error);
-return response.status(422).json(error);
-}
-
-
-});
+app.get("/todos", function (request, response) {
+  console.log("todo list",request.body);
+})
 
 app.get("/todos/:id", async function (request, response) {
   try {
--- models/todo.js
+++ models/todo.js
@@ -15,6 +15,11 @@ module.exports = (sequelize, DataTypes) => {
       return this.create({ title: title, dueDate: dueDate, completed: false });
     }
 
+
+    static getTodos()
+    {
+      return this.findAll();
+    }
     markAsCompleted() {
       return this.update({ completed: true });
     }
