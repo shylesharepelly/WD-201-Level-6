9f5ef6f707a89a09aa7f31d7cf2952899c65e61a
Shylesh <shylesharepelly@gmail.com>
2022-12-20 18:58:42 +0530

    final level-9

--- __tests__/todos.js
+++ __tests__/todos.js
@@ -60,6 +60,7 @@ describe("Todo Application", function () {
       .put(`/todos/${latestTodo.id}`)
       .send({
         _csrf: csrfToken,
+        completed:false,
       });
 
     const parsedUpdateResponse = JSON.parse(markCompleteResponse.text);
